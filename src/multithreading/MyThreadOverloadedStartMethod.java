276b45211d971163595ae50fbef356d3ea0d869e
Shivam Patil <shivampatil727@gmail.com>
2023-08-13 21:24:42 +0530

    Overloading start method

---
+++ src/multithreading/MyThreadOverloadedStartMethod.java
@@ -0,0 +1,24 @@
+package multithreading;
+
+public class MyThreadOverloadedStartMethod extends Thread{
+
+
+    public void run() {
+        System.out.println("run method");
+    }
+
+
+    public void start() {
+        System.out.println("start methd");
+
+    }
+
+    public static void main(String[] args) {
+        MyThreadOverloadedStartMethod myThreadWithoutRunMethod = new MyThreadOverloadedStartMethod();
+        myThreadWithoutRunMethod.start();
+    }
+}
+
+//start method - as normal method call
+
+
