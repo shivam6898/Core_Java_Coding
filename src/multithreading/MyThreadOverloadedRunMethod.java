21f9692991cdc2ee287fddad0987149568988c65
Shivam Patil <shivampatil727@gmail.com>
2023-08-13 21:21:42 +0530

    Overloading Run method

---
+++ src/multithreading/MyThreadOverloadedRunMethod.java
@@ -0,0 +1,24 @@
+package multithreading;
+
+public class MyThreadOverloadedRunMethod extends Thread{
+
+
+    public void run() {
+        System.out.println("no-arg");
+    }
+
+
+    public void run(int i) {
+        System.out.println("with-arg");
+
+    }
+
+    public static void main(String[] args) {
+        MyThreadOverloadedRunMethod myThreadWithoutRunMethod = new MyThreadOverloadedRunMethod();
+        myThreadWithoutRunMethod.start();
+    }
+}
+
+//no-arg
+
+
