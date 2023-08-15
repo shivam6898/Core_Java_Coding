96c6aaf2a2107dc8431230163c0c2527faa67109
Shivam Patil <shivampatil727@gmail.com>
2023-08-16 01:50:12 +0530

    gets the first result from future

---
+++ src/multithreading/MultipleAnyCallableRunner.java
@@ -0,0 +1,27 @@
+package multithreading;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+ class MultipleAnyCallableRunner {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		ExecutorService executorService = Executors.newFixedThreadPool(3);
+		
+		List<CallableTask> tasks = List.of(
+				new CallableTask("shivam"),
+				new CallableTask("prabal"),
+				new CallableTask("vibhansu"));
+
+		//invokeAny Method gets the first completed task as a result
+		String result = executorService.invokeAny(tasks);
+		
+		System.out.println(result);
+
+		executorService.shutdown();
+	}
+
+}
