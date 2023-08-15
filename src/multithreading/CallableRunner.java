15bf0e1dba6849495f980dd6f815d364253685cf
Shivam Patil <shivampatil727@gmail.com>
2023-08-15 23:59:26 +0530

    Callable service

---
+++ src/multithreading/CallableRunner.java
@@ -0,0 +1,51 @@
+package multithreading;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+class CallableTask implements Callable<String> {
+	
+	private String name;
+
+	public CallableTask(String name) {
+		this.name = name;
+	}
+
+
+	//here thread returns something after execution
+	@Override
+	public String call() throws Exception {
+		Thread.sleep(1000);
+		return "Hello " + name;
+	}
+	
+}
+
+public class CallableRunner {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		ExecutorService executorService = Executors.newFixedThreadPool(1);
+		
+		Future<String> welcomeFuture = 
+				executorService.submit(new CallableTask("shivam"));
+		
+		System.out.print("\n new CallableTask(\"in28Minutes\") executed\n");
+		
+		String welcomeMessage = welcomeFuture.get();
+		
+		System.out.println(welcomeMessage);
+		
+		System.out.print("\n Main completed");
+		executorService.shutdown();
+
+	}
+
+}
+
+// new CallableTask("in28Minutes") executed
+//		 Hello shivam
+//
+//		 Main completed
