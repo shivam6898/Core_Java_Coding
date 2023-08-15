12b28961f3b666de4d44392b1d64b2f10a638c16
Shivam Patil <shivampatil727@gmail.com>
2023-08-16 01:54:04 +0530

    Complete all task

---
+++ src/multithreading/MultipleCallableRunner.java
@@ -0,0 +1,26 @@
+package multithreading;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class MultipleCallableRunner {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		ExecutorService executorService = Executors.newFixedThreadPool(3);
+		
+		List<CallableTask> tasks = List.of(new CallableTask("in28Minutes"), 
+				new CallableTask("Ranga"),  new CallableTask("Adam"));
+		//waiting for all the task to complete
+		List<Future<String>> results = executorService.invokeAll(tasks);
+		
+		for(Future<String> result:results) {
+			System.out.println(result.get());
+		}
+
+		executorService.shutdown();
+	}
+
+}
