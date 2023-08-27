f64078af738c92f77597a7142bfc2f859f871f5a
shivam patil <shivampatil727@gmail.com>
2023-08-27 21:03:33 +0530

    test

---
+++ src/Java_Coding/Str.java
@@ -0,0 +1,12 @@
+package Java_Coding;
+
+public class Str {
+    public static void main(String[] args) {
+        String s = "  abc  def ";
+
+        s = s.trim();
+
+        System.out.println(s);
+        System.out.println(s.length());
+    }
+}
