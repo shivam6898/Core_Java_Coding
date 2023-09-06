a6ebe60428851ed69c38f14c63c10084efcd2f83
Shivam Patil <shivampatil727@gmail.com>
2023-09-06 17:51:17 +0530

    Interface

--- src/corejava/Interf.java
+++ src/corejava/Interf.java
@@ -2,7 +2,39 @@ package corejava;
 
 public interface Interf {
 
-    public static void main(String[] args) {
-        System.out.println("execution with interface");
+    //abstact method by default puublic abstract
+    public abstract void show();
+
+    //default method with default Keyword
+    default void display(){
+        System.out.println("default method in interface");
+        print();
+    }
+
+    private void print(){
+        System.out.println("private method");
+    }
+
+
+    //static method
+     static void main(String[] args) {
+        Impl impl = new Impl();
+        impl.show();
+        impl.display();
+        System.out.println("static main method execution with interface");
     }
 }
+
+
+class Impl implements Interf{
+
+    @Override
+    public void show() {
+        System.out.println("implemented method");
+    }
+}
+
+//implemented method
+//default method in interface
+//private method
+//static main method execution with interface
