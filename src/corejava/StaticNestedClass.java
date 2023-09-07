519833e30c5e51dbe49022e31d80ddf72b8274b3
Shivam Patil <shivampatil727@gmail.com>
2023-09-08 04:45:46 +0530

    static nested class

--- src/corejava/StaticNestedClass.java
+++ src/corejava/StaticNestedClass.java
@@ -12,10 +12,10 @@ class OuterClass {
             // System.out.println("StaticNestedClass: outerInstanceVar = " + outerInstanceVar); // This would result in a compilation error
         }
     }
-}
- class StaticNestedClass_Main {
+
     public static void main(String[] args) {
         OuterClass.StaticNestedClass nestedObj = new OuterClass.StaticNestedClass();
         nestedObj.display();
     }
 }
+
