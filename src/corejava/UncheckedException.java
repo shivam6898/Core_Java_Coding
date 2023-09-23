c203a65f0876ed6c2bd3dc9d2866bcc75c67e47d
Shivam Patil <shivampatil727@gmail.com>
2023-09-23 15:27:33 +0530

    custom exception

--- src/corejava/UncheckedException.java
+++ src/corejava/UncheckedException.java
@@ -1,4 +1,24 @@
 package corejava;
 
+//create custom exception
+class NegativeNumberException extends RuntimeException{
+    public NegativeNumberException(String number_is_negative) {
+        super(number_is_negative);
+    }
+}
+
+
 public class UncheckedException {
+
+    public static int cube(int a ) throws NegativeNumberException {
+        if (a>0){
+            return a*a;
+        }else{
+            throw new NegativeNumberException("NUMBER IS NEGATIVE");
+        }
+    }
+
+    public static void main(String[] args) {
+        cube(-10);
+    }
 }
