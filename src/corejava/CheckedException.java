8727dcab9114360deb943512f817c0febccc9500
Shivam Patil <shivampatil727@gmail.com>
2023-09-23 15:34:39 +0530

    checked exception

---
+++ src/corejava/CheckedException.java
@@ -0,0 +1,25 @@
+package corejava;
+
+class AgeException extends Exception{
+    public AgeException(String messaage){
+        super(messaage);
+    }
+}
+
+public class CheckedException {
+    public static String checkAge(int age) throws AgeException {
+        if (age >18){
+            return "registered";
+        }else {
+            throw new AgeException("AGE IS BELOW 18");
+        }
+    }
+    public static void main(String[] args) {
+        //here try catch is mandatory must be handeled by programmer
+        try {
+            checkAge(10);
+        } catch (AgeException e) {
+            e.printStackTrace();
+        }
+    }
+}
