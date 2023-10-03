4b1c79fb4521819de6457de72f27ca0207ffc66c
Shivam Patil <shivampatil727@gmail.com>
2023-10-03 21:14:50 +0530

    minor fix

--- src/Java_Coding/PrintFibonacciRecursive.java
+++ src/Java_Coding/PrintFibonacciRecursive.java
@@ -1,22 +1,20 @@
 package Java_Coding;
 
 public class PrintFibonacciRecursive {
-
+    public static void main(String args[]) {
+        int seqLength = 10;
+        System.out.print("A Fibonacci sequence of " + seqLength + " numbers: ");
+        for (int i = 0; i < seqLength; i++) {
+            System.out.println(fibonacci(i));
+        }
+    }
     public static int fibonacci(int count) {
-        if (count <= 1)
+        if (count <= 1){
             return count;
-
+        }
         return fibonacci(count - 1) + fibonacci(count - 2);
     }
 
-    public static void main(String args[]) {
-        int seqLength = 10;
 
-        System.out.print("A Fibonacci sequence of " + seqLength + " numbers: ");
-
-        for (int i = 0; i < seqLength; i++) {
-            System.out.print(fibonacci(i) + " ");
-        }
-    }
 
 }
